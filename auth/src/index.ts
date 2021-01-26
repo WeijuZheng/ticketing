eeb95411259c28b43ff70915d5fe53e8839ab451
weiju <weijuzheng1995@gmail.com>
2021-01-25 16:11:28 -0800

    finished signup and signin route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
-import mongoose, { connect } from 'mongoose';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,14 @@ import { errorHandler } from './middlewares/errorHandler';
 import { NotFoundError } from './errors/notFoundError';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,6 +32,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try {
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
             useNewUrlParser: true,
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/requestValidationError';
 import { BadRequestError } from '../errors/badRequestError';
 
 const router = express.Router();
@@ -16,13 +18,8 @@ router.post('/api/users/signup',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
-
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email: email });
 
@@ -32,6 +29,17 @@ router.post('/api/users/signup',
 
         const user = User.build({ email, password });
         await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!);
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(user);
     });
