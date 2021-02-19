627d11eb4593efcf77633e1f478426bc0df6e4cb
weiju <weijuzheng1995@gmail.com>
2021-02-18 17:05:14 -0800

    payment service init setup, listeners, create route

---
+++ payments/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@wztickets/common';
+
+import { createChargeRouter } from './routes/create';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false,
+        // when running jest, process.env.NODE_ENV will be set to 'test
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+app.use(currentUser);
+
+app.use(createChargeRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
